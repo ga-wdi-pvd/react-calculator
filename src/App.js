99752186bb93d532df07e049e33dc37a22970102
murphypicard <ayaghsizian@gmail.com>
2017-02-02 22:46:13 -0500

    ara react calculator done wet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Add from './Add';
+import Sub from './Sub';
+import Tim from './Tim';
+import Div from './Div';
 import './App.css';
 
 
 class App extends Component {
 
-
-
-
   render() {
-
     return (
-      <div>
-        <h1> Apppp Parent stuuuuffffff </h1>
-        <Add/>
-      </div>
-    );
+            <div>
+                  <h1> Ara finally made a calculator with REACT </h1>
 
+                  <Add/>
+                  <Sub/>
+                  <Tim/>
+                  <Div/>
+            </div>
+    );
   }//render
 }//app
 
