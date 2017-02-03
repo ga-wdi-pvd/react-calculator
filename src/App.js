4aa43a745038967e654842551470de2855fac9ab
murphypicard <ayaghsizian@gmail.com>
2017-02-02 21:02:43 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Add from './Add';
 import './App.css';
+
 
 class App extends Component {
 
+
+
+
   render() {
+
     return (
-      <Calculator />
+      <div>
+        <h1> Apppp Parent stuuuuffffff </h1>
+        <Add/>
+      </div>
     );
-  }
-}
+
+  }//render
+}//app
 
 export default App;
